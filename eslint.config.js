c587013392d496905abef1b61855dfd02d72f050
The-Aldi-Tri <the.aldi.tri@gmail.com>
2025-01-15 09:57:56 +0000

    lint and format

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
-
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ];
